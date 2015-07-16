d4a10ea3ec7b54221638919b9ab8266899807bfa
KONG <nuttanun77@hotmail.com>
2015-07-16 22:58:54 +0700

    new format and  add wait element

--- JJJud.js
+++ JJJud.js
@@ -1,144 +1,155 @@
 (function($){
-	var settings = "";
-	$.fn.JJJud = function(options){
-		// This is the easiest way to have default options.
-        settings = $.extend( {}, $.fn.JJJud.defaults, options );
-        var loop_on_cut = $.fn.JJJud.loop;
+    $.JJJud = function(el, options){
+        // To avoid scope issues, use '_this' instead of 'this'
+        // to reference this class from internal events and functions.
+        var _this = this;
+        
+        // Access to jQuery and DOM versions of element
+        _this.$el = $(el);
+        _this.el = el;
 
-		return this.each(function(a,e) {
-			// init(this,settings);
-			if ((typeof options == 'object') || (options == undefined)) {
-				$.fn.JJJud.the_selected = $(e);
-			}
+        _this.initials = {
+        	waitElement: null
+        }
+        
+        $.extend(_this, _this.initials);
 
-	        // Call our cutWord function.
-	        if(settings["elmVisibleOnLoop"] != ""){
-	        	// $.fn.JJJud.setLoop();
-	        	// loop_on_cut = setInterval(function(){
-	        	// 	if($(settings["elmVisibleOnLoop"]).is(":visible")){
-		        // 		$.proxy($.fn.JJJud.cutWord(), this);
-		        // 		clearInterval(loop_on_cut);
-		        // 	}
-	        	// }, 1000);
+        // Add a reverse reference to the DOM object
+        _this.$el.data("JJJud", _this);
 
-	        	// loop_on_cut = setInterval(function(){
-	        	// 	if($(settings["elmVisibleOnLoop"]).is(":visible")){
-		        // 		$.proxy($.fn.JJJud.cutWord(), this);
-		        // 		clearInterval(loop_on_cut);
-		        // 	}
-	        	// }, 1000);
-				// elm = loop_on_cut;
-	        	// elm = setInterval(function(){
-	        	// 	$.proxy($.fn.JJJud.cutWord,elm);
-	        	// 	clearInterval(loop_on_cut);
-	        	// }, 1000);
-				var elm = $.fn.JJJud;
-				console.log("settings:",elm)
-	        	loop_on_cut =  setInterval($.proxy(function () {
-	        			if($(settings["elmVisibleOnLoop"]).is(":visible")){
-				            $.fn.JJJud.cutWord();
-				            clearInterval(loop_on_cut);
-				        }
-			        }, elm), 1000);
+        _this.countVowelToneMarks = function(inputText) {
+	        var textCount = new Array(/ิ/g,/ี/g,/ึ/g,/ื/g,/ุ/g,/ู/g,/่/g,/้/g,/๊/g,/๋/g,/ั/g,/์/g,/็/g);
+	        var sumCount = 0;
+	        for (var i in textCount) {
+	            sumCount += (inputText.match(textCount[i])||[]).length;
+	        }
+	        return sumCount;
+	    }
 
-		    }else{
-		    	$.fn.JJJud.cutWord();
-		    }
+		_this.getText = function(){
+			var _this = this;
 
-		});
-	};
+			return _this.$el.text();
+		}
 
-	$.fn.JJJud.defaults = {
+		_this.getHeight = function(){
+
+			var _this = this;
+			return _this.$el.height();
+		}
+
+		_this.getFontSize = function(){
+			var _this = this;
+
+			return parseInt(_this.$el.css("font-size"));
+		}
+
+		_this.getMaxHeight = function(){
+
+			var _this = this;
+
+			var tempText = _this.getText();
+			_this.$el.text("aa");
+			var lineheight = _this.getHeight();
+			_this.$el.text(tempText);
+			return (_this.settings["max_rows"] * lineheight) + (lineheight/2);
+		}
+		 _this.getLineHeight = function(){
+
+		 	var _this = this;
+
+	        var tempText = _this.getText();
+	        _this.$el.text("0");
+	        var lineheight = _this.getHeight();
+	        _this.$el.text(tempText);
+	        return lineheight;
+	    }
+
+        _this.cutWord = function(){
+
+	    	var _this = this;
+
+			var elmHeightRows = 0;
+			var whileHeight = _this.getHeight();
+			var counter = 0;
+			// var textLength = $(this).html().length;
+			var lengthWordRow = _this.getText().length;
+			var lineHeight = _this.getLineHeight();
+	        var lineCount =  (_this.getHeight() / lineHeight)-1;
+	        //var tempVerticalPadding = 0;
+	        if(lineCount == 0){
+	        	lineCount = 1;
+	        }
+	        var textNumberNeed = ((lengthWordRow * _this.settings["max_rows"]) / lineCount);
+	        var textNeed = _this.getText().substr(0,parseInt(textNumberNeed));
+
+	        // console.log("need",textNeed);
+	        if(_this.getMaxHeight() != 0 || _this.getMaxHeight() != ""){
+	        var offset = _this.countVowelToneMarks(textNeed);
+	        var textOffset = _this.getText().substr(0,parseInt(textNumberNeed) + offset);
+
+	        // console.log("offset",textOffset);
+	        _this.$el.html(textOffset);
+
+	        _this.$el.html(textNeed);
+
+	            //console.log(lineHeight);
+				while (whileHeight >= _this.getMaxHeight() && counter < 100) {
+					counter++;
+					var lengthWord = _this.getText().length;
+				    var cutString = _this.getText().substring(0, lengthWord - (counter == 1 ? 3 : 6)) + "...";
+				    _this.$el.html(cutString);
+					whileHeight = _this.getHeight();
+	                //console.log("cut","ss");
+				}
+	            if (counter == 100) {
+	                console.error("JJJud Calculate Error");
+	            }
+				_this.settings["onComplete"](_this.$el);
+	        }
+
+		}
+
+		_this.waitElementToShow = function(){
+			var _this = this;
+
+			_this.waitElement = setInterval(function(){
+								if($(_this.settings["waitElement"]).is(":visible")){
+									_this.cutWord();
+									clearInterval(_this.waitElement);
+								}
+						},_this.settings["waitTime"]);
+		}
+        
+        _this.init = function(){
+        	
+        	var _this = this;
+            _this.settings = $.extend({},$.JJJud.defaultOptions, options);
+            // Put your initialization code here
+            if (_this.settings["waitElement"] != "") {
+            	_this.waitElementToShow();
+	        }else{
+	        	_this.cutWord();
+	        }
+        };
+        
+        // Run initializer
+        _this.init();
+    };
+    
+    $.JJJud.defaultOptions = {
             "max_rows": 2, // Max row
             "onComplete": $.noop,
-            "elmVisibleOnLoop": ""
+            "waitElement": "",
+            "waitTime": 200
     };
-
-    $.fn.JJJud.cutWord = function(elm){
-		var elmHeightRows = 0;
-		var whileHeight = this.getHeight();
-		var counter = 0;
-		// var textLength = $(this).html().length;
-		var lengthWordRow = this.getText().length;
-		var lineHeight = this.getLineHeight();
-        var lineCount =  (this.getHeight() / lineHeight)-1;
-        //var tempVerticalPadding = 0;
-        if(lineCount == 0){
-        	lineCount = 1;
-        }
-        var textNumberNeed = ((lengthWordRow*settings["max_rows"])/lineCount);
-        var textNeed = this.getText().substr(0,parseInt(textNumberNeed));
-
-        // console.log("need",textNeed);
-        if(this.getMaxHeight() != 0 || this.getMaxHeight() != ""){
-        var offset = this.countVowelToneMarks(textNeed);
-        var textOffset = this.getText().substr(0,parseInt(textNumberNeed) + offset);
-
-        // console.log("offset",textOffset);
-        console.log("this.the_selected:",this.the_selected)
-        this.the_selected.html(textOffset);
-
-        this.the_selected.html(textNeed);
-
-            //console.log(lineHeight);
-			while (whileHeight >= this.getMaxHeight() && counter < 100) {
-				counter++;
-				var lengthWord = this.getText().length;
-			    var cutString = this.getText().substring(0, lengthWord - (counter == 1 ? 3 : 6)) + "...";
-			    this.the_selected.html(cutString);
-				whileHeight = this.getHeight();
-                //console.log("cut","ss");
-			}
-            if (counter == 100) {
-                console.error("JJJud Calculate Error");
-            }
-			settings["onComplete"](this.the_selected);
-        }
-	}
-    $.fn.JJJud.countVowelToneMarks = function(inputText) {
-        var textCount = new Array(/ิ/g,/ี/g,/ึ/g,/ื/g,/ุ/g,/ู/g,/่/g,/้/g,/๊/g,/๋/g,/ั/g,/์/g,/็/g);
-        var sumCount = 0;
-        for (var i in textCount) {
-            sumCount += (inputText.match(textCount[i])||[]).length;
-        }
-        return sumCount;
-    }
-
-	$.fn.JJJud.getText = function(){
-		return this.the_selected.text();
-	}
-
-	// $.fn.JJJud.setLoopInteval = function(){
-	// 	var _this = $(this);
-	// 	// console.log("this:",$(this))
-	// 	if($(settings["elmVisibleOnLoop"]).is(":visible")){
-//       		$(this).cutWord();
-//       		clearInterval(loop_on_cut);
-//       	}
-	// }
-
-	$.fn.JJJud.getHeight = function(){
-        //var padding = parseInt(this.the_selected.css("padding-top")) + parseInt(this.the_selected.css("padding-bottom"));
-        //console.log(padding);
-		return this.the_selected.height();
-	}
-
-	$.fn.JJJud.getFontSize = function(){
-		return parseInt(this.the_selected.css("font-size"));
-	}
-
-	$.fn.JJJud.getMaxHeight = function(){
-		var tempText = this.getText();
-		this.the_selected.text("aa");
-		var lineheight = this.getHeight();
-		this.the_selected.text(tempText);
-		return (settings["max_rows"] * lineheight) + (lineheight/2);
-	}
-	 $.fn.JJJud.getLineHeight = function(){
-        var tempText = this.getText();
-        this.the_selected.text("0");
-        var lineheight = this.getHeight();
-        this.the_selected.text(tempText);
-        return lineheight;
-    }
+    
+    $.fn.JJJud = function(options){
+        return this.each(function(){
+            (new $.JJJud(this, options));
+        });
+    };
+    
 })(jQuery);
+
+
